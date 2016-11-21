38c623eea36ed6bc71cca5c9e30f5d33a88835f1
Christian Panadero <panavtec@gmail.com>
2016-11-21 21:17:14 +0000

    fix typo

--- keyboards/s60-x/keymaps/panavtec/keymap.c
+++ keyboards/s60-x/keymaps/panavtec/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = KEYMAP(
     // ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
     // │ ESC   │  1    │  2    │  3    │  4    │  5    │  6    │  7    │  8    │  9    │  0    │  -    │  =    │▒▒▒▒▒▒▒│ BKSPC │
-         KC_ESC,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,  KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 ,KC_MINS, KC_EQL,  KC_NO,KC_BSPC, \
+         KC_ESC,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,  KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 ,KC_MINS, KC_EQL, KC_NO ,KC_BSPC, \
     // ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┤
     // │ TAB   │  Q    │  W    │  E    │  R    │  T    │  Y    │  U    │  I    │  O    │  P    │  [    │  ]    │       \       │
          KC_TAB,  KC_Q ,  KC_W ,  KC_E ,  KC_R ,  KC_T ,  KC_Y ,  KC_U ,  KC_I ,  KC_O ,  KC_P ,KC_LBRC,KC_RBRC,    KC_BSLS    , \
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_NO ,KC_TRNS,KC_TRNS,KC_TRNS,KC_VOLD,KC_SPC,KC_PGDN,TO(MOUSE),KC_TRNS,KC_TRNS,KC_APP, KC_NO ,KC_RSFT, KC_NO , \
     // ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┤
     // │███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████████████│
-        KC_TRNS,KC_TRNS,KC_TRNS,                        KC_TRNS,                        KC_TRNS,KC_TRNS KC_TRNS,    KC_TRNS),    \
+        KC_TRNS,KC_TRNS,KC_TRNS,                        KC_TRNS,                        KC_TRNS,KC_TRNS,KC_TRNS,    KC_TRNS),    \
     // └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
 
     [MOUSE] = KEYMAP(
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, TO(IJ),KC_TRNS,KC_TRNS,KC_VOLU,KC_TRNS,KC_PGUP,KC_LEFT,  F(30),KC_RGHT,KC_TRNS,KC_TRNS,KC_TRNS,     KC_TRNS   , \
     // ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
     // │███████│▒▒▒▒▒▒▒│███████│███████│███████│ VOL DO│  SPC  │ PG DO │  M    │███████│███████│  /    │▒▒▒▒▒▒▒│ RSHFT │▒▒▒▒▒▒▒│
-        KC_TRNS, KC_NO ,KC_TRNS,KC_TRNS,KC_TRNS,KC_VOLD,KC_SPC,KC_PGDN,TO(MOUSE),KC_TRNS,KC_TRNS, KC_APP,  KC_NO,KC_RSFT, KC_NO , \
+        KC_TRNS, KC_NO ,KC_TRNS,KC_TRNS,KC_TRNS,KC_VOLD,KC_SPC,KC_PGDN,TO(MOUSE),KC_TRNS,KC_TRNS, KC_APP,  KC_NO,KC_RSFT, KC_NO, \
     // ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────────────┤
     // │███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████│███████████████│
         KC_TRNS,KC_TRNS,KC_TRNS,                        KC_TRNS,                        KC_TRNS,KC_TRNS,KC_TRNS,    KC_TRNS),    \
